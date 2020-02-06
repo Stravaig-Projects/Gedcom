a2090eb022b220efd7e02578a1a09f3c16676081
Colin Mackay <github@colinmackay.scot>
2020-02-06 18:08:57 +0000

    Add check for GEDCOM alpha

---
+++ src/Stravaig.Gedcom.UnitTests/Extensions/CharExtensions_IsGedcomAlphaTests.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using Shouldly;
+using Stravaig.Gedcom.Extensions;
+
+namespace Stravaig.Gedcom.UnitTests.Extensions
+{
+    [TestFixture]
+    public class CharExtensions_IsGedcomAlphaTests
+    {
+        [Test]
+        [TestCaseSource(nameof(ValidAlphaCharacters))]
+        public void IsGedcomAlpha_ForValidCharacters_ReturnsTrue(char alphaChar)
+        {
+            alphaChar.IsGedcomAlpha().ShouldBeTrue();
+        }
+
+        [Test]
+        [TestCaseSource(nameof(Numbers))]
+        public void IsGedcomAlpha_ForNumbers_ReturnsFalse(char alphaChar)
+        {
+            alphaChar.IsGedcomAlpha().ShouldBeFalse();
+        }
+
+        [Test]
+        [TestCaseSource(nameof(Symbols))]
+        public void IsGedcomAlpha_ForSymbols_ReturnsFalse(char alphaChar)
+        {
+            alphaChar.IsGedcomAlpha().ShouldBeFalse();
+        }
+        
+        [Test]
+        [TestCaseSource(nameof(AccentedLetters))]
+        public void IsGedcomAlpha_ForAccentedLetters_ReturnsFalse(char alphaChar)
+        {
+            alphaChar.IsGedcomAlpha().ShouldBeFalse();
+        }
+        
+        private static IEnumerable<char> ValidAlphaCharacters()
+        {
+            for (char c = 'a'; c <= 'z'; c++)
+                yield return c;
+            for (char c = 'A'; c <= 'Z'; c++)
+                yield return c;
+            yield return '_';
+        }
+
+        private static IEnumerable<char> Numbers()
+        {
+            for (char c = '0'; c <= '9'; c++)
+                yield return c;
+        }
+
+        private static IEnumerable<char> Symbols()
+        {
+            return "!£$%^&*()-+={}[]:@~;'#<>?,./\\|\"`¬";
+        }
+
+        private static IEnumerable<char> AccentedLetters()
+        {
+            for (char c = (char) 0xC0; c <= (char) 0xFF; c++)
+                yield return c;
+        }
+        
+    }
+}
--- src/Stravaig.Gedcom/Extensions/CharExtensions.cs
+++ src/Stravaig.Gedcom/Extensions/CharExtensions.cs
@@ -4,7 +4,12 @@ namespace Stravaig.Gedcom.Extensions
     {
         private const char Zero = (char) 0x30;
         private const char Nine = (char) 0x39;
-
+        private const char LowerAlpha = (char) 0x61; 
+        private const char LowerZulu = (char) 0x7A; 
+        private const char UpperAlpha = (char) 0x41; 
+        private const char UpperZulu = (char) 0x5A; 
+        private const char Underscore = (char) 0x5F; 
+        
         /// <summary>
         /// Determines if the target value is a GEDCOM Digit.
         /// </summary>
@@ -20,5 +25,25 @@ namespace Stravaig.Gedcom.Extensions
         {
             return (target >= Zero) && (target <= Nine);
         }
+        
+        /// <summary> 
+        /// Determines if the target value is a GEDCOM alpha.
+        /// </summary> 
+        /// <param name="target">The target character to test.</param> 
+        /// <returns>true if the target is a GEDCOM alpha.</returns> 
+        /// <remarks> 
+        /// alpha:= 
+        ///   [(0x41)-(0x5A) | (0x61)-(0x7A) | (0x5F) ] 
+        ///   where: 
+        ///   (0x41)-(0x5A)=A to Z 
+        ///   (0x61)-(0x7A)=a to z 
+        ///   (0x5F)=(_) underscore 
+        /// </remarks> 
+        public static bool IsGedcomAlpha(this char target) 
+        { 
+            return (target >= LowerAlpha && target <= LowerZulu) || 
+                   (target >= UpperAlpha && target <= UpperZulu) || 
+                   target == Underscore; 
+        } 
     }
 }
