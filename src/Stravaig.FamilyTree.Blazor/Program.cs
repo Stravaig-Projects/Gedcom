c19f30fb583b7100577eb6113921bc9a5a973fda
Colin Mackay <github@colinmackay.scot>
2020-06-14 01:12:14 +0100

    Update bootstrap & remove examples

--- src/Stravaig.FamilyTree.Blazor/Program.cs
+++ src/Stravaig.FamilyTree.Blazor/Program.cs
@@ -17,7 +17,10 @@ namespace Stravaig.FamilyTree.Blazor
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
 
             await builder.Build().RunAsync();
         }
