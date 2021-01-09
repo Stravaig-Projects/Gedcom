ff4743dabdfee06a5f2ad7a44df97b3421b43c20
Colin Mackay <github@colinmackay.scot>
2021-01-09 14:02:10 +0000

    #72 Fix up issues in Stravaig.Gedcom.PowerShell

--- src/Stravaig.Gedcom.PowerShell/PSGedcomDatabase.cs
+++ src/Stravaig.Gedcom.PowerShell/PSGedcomDatabase.cs
@@ -9,14 +9,12 @@ namespace Stravaig.Gedcom.PowerShell
     // This is the PowerShell standard naming.
     public class PSGedcomDatabase
     {
-        private readonly GedcomDatabase _database;
         private readonly Lazy<PSGedcomIndividual[]> _individualRecords;
         
         public PSGedcomDatabase(GedcomDatabase database)
         {
-            _database = database;
             _individualRecords = new Lazy<PSGedcomIndividual[]>(
-                () => _database.IndividualRecords
+                () => database.IndividualRecords
                     .Select(kvp => kvp.Value)
                     .OrderByStandardSort()
                     .Select(ir => new PSGedcomIndividual(ir))
