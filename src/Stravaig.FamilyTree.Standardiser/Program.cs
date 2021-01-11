b5491065281abc954cbacc4f0d27831fe800ba97
Colin Mackay <github@colinmackay.scot>
2021-01-11 21:00:45 +0000

    #76 Remove unused using statements

--- src/Stravaig.FamilyTree.Standardiser/Program.cs
+++ src/Stravaig.FamilyTree.Standardiser/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using CommandLine;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
--- src/Stravaig.FamilyTreeGenerator/Program.cs
+++ src/Stravaig.FamilyTreeGenerator/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using CommandLine;
 using Humanizer;
 using Microsoft.Extensions.DependencyInjection;
